a477138f678fde7a7f4a8e49de017deebca8e126
Abdul Waqar <63345475+abdulwaqar844@users.noreply.github.com>
2023-09-25 04:12:31 -0700

    updated final

--- final.py
+++ final.py
@@ -59,4 +59,5 @@ async def upload_file(pdf_file: UploadFile):
     return {"message": results}
 
 if __name__ == "__main__":
-    uvicorn.run("final:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run("final:app", host="0.0.0.0", port=port)
